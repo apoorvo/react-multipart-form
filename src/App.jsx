1792709e42f3f798b1a243298f920f5ea63aeeb6
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2022-01-15 00:01:56 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen flex justify-center flex-col items-center h-screen ">
+      <div className="w-2/5 p-4 h-3/5">
+        <h1 className="">Lorem Ipsum</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
